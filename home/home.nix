ac706899c092ad016290fdb96a01d5701db95c01
Mouthless-Stoat <nphuyanh@outlook.com>
2025-10-17 22:03:54 -0300

    feat: move packages to another file

--- home/home.nix
+++ home/home.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -19,24 +15,7 @@
     };
   };
 
-  home.packages = with pkgs; [
-    fzf
-    ripgrep
-    git-credential-manager
-    pass
-    gnupg
-    steam-tui
-    steamcmd
-
-    typst
-    tinymist
-
-    fd
-    kitty
-    rofi
-    xwayland-satellite
-    brightnessctl
-  ];
+  home.packages = import ./packages.nix pkgs;
 
   programs.bash.enable = true;
 
---
+++ home/packages.nix
@@ -0,0 +1,19 @@
+pkgs: with pkgs; [
+  fzf
+  ripgrep
+  git-credential-manager
+  pass
+  gnupg
+  steam-tui
+  steamcmd
+
+  typst
+  tinymist
+
+  fd
+  kitty
+  rofi
+  xwayland-satellite
+  brightnessctl
+
+]
