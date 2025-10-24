4829e834ef0f4540ba985b221ca491a5068df075
Mouthless-Stoat <nphuyanh@outlook.com>
2025-10-24 19:10:19 -0300

    feat: firefox floating window fix

--- home/mouthless/niri/default.nix
+++ home/mouthless/niri/default.nix
@@ -5,6 +5,20 @@
     hotkey-overlay.hide-not-bound = true;
     hotkey-overlay.skip-at-startup = true;
 
+    window-rules = [
+      {
+        matches = [
+          {
+            app-id = "firefox$";
+            title = "^Picture-in-Picture$";
+          }
+        ];
+        default-column-width.proportion = 1. / 4.;
+        default-window-height.proportion = 1. / 4.;
+        open-floating = true;
+      }
+    ];
+
     layout = {
       default-column-width.proportion = 1. / 2.;
       default-column-display = "tabbed";
