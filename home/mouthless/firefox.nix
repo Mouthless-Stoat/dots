b276cd75f5e2597f548e3397ee3a7bed80c1ad82
Mouthless-Stoat <nphuyanh@outlook.com>
2025-10-29 11:59:17 -0300

    fet: update firefox setting

--- home/mouthless/firefox.nix
+++ home/mouthless/firefox.nix
@@ -5,6 +5,18 @@
     profiles.personal = {
       isDefault = true;
       extensions.force = true;
+      settings = {
+        browser.newtabpage.activity-stream.feeds.section.topstories = false;
+        browser.newtabpage.activity-stream.section.highlights.includePocket = false;
+        browser.newtabpage.activity-stream.showSponsored = false;
+        extensions.pocket.enabled = false;
+      };
+      userChrome = "
+        /* userChrome.css */
+        :root {
+            min-width: 1rem !important;
+        }
+      ";
     };
   };
 }
